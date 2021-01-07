52bc9eb3f44b604536dfdbbff33aae9b7c147094
unknown <kevensantana40@gmail.com>
2021-01-07 00:15:15 -0300

    GET /orders

---
+++ backend/src/main/java/com/devknow/dsdelivery/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.devknow.dsdelivery.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devknow.dsdelivery.dto.OrderDTO;
+import com.devknow.dsdelivery.entities.Order;
+import com.devknow.dsdelivery.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
